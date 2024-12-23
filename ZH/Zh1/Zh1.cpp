58d017bf3ee28e04391762c2c9b31702d7b23e4a
SATA <sandor.tardi@sensirion.com>
2024-12-23 23:06:43 +0100

    Add Zh1

---
+++ ZH/Zh1/Zh1.cpp
@@ -0,0 +1,33 @@
+﻿/*
+ 1. Írasd ki 0-tól 10 000-ig azokat a számokat, melyek utolsó számjegye megegyezik négyzetének első számjegyével! Pl.:246 
+ */
+
+#include <iostream>
+
+int get_first_digit(int i);
+int get_last_digit(int i);
+
+int main()
+{
+    for (int i = 0; i <= 10000; i++)
+    {
+      int last_digit = get_last_digit(i);
+      int square = i * i;
+      int first_digit_of_square = get_first_digit(square);
+      if (last_digit == first_digit_of_square)
+      {
+        std::cout << i << std::endl;
+      }
+    }
+}
+
+int get_first_digit(int i)
+{
+  while (i >= 10) i /= 10;
+  return i;
+}
+
+int get_last_digit(int i)
+{
+  return i % 10;
+}

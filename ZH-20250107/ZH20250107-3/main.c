eabb25f95c814bddcea870df799a55db206e0e94
ujcsi <ujcsi@satrax.hu>
2025-01-07 23:26:02 +0100

    zh 3.feladat

---
+++ ZH-20250107/ZH20250107-3/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fg (int x,int y){
+    int max,i;
+    if (x>y){
+        max=x;
+    }
+    if (y>x){
+        max=y;
+    }
+    for (i=1;i<=max;i++){
+        if (((x%i)!=0)&&((y%i)!=0)){
+            return i;
+        }
+    }
+}
+
+int main()
+{
+
+
+
+
+    int elso=21;
+    int masodik=33;
+    int c;
+    c=fg(elso,masodik);
+    printf("%d ",c);
+    return 0;
+}
